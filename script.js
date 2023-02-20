56485d8744c71aff86658d756019b7435fb2ada2
macubetech <116107346+macubetech@users.noreply.github.com>
2023-02-20 14:53:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var menuicon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuicon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
+
+
+var preloader = document.getElementById('loading');
+
+function myFunction(){
+    preloader.style.display = 'none';
+}
